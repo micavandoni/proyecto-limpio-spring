21da670616d3288e87cae0cad6dffcb3cd508145
Diego salas <diegonsalas@gmail.com>
2020-05-14 21:39:10 -0300

    clase propiedad

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Propiedad.java
@@ -0,0 +1,64 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Propiedad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String direccion;
+    private String descripcion;
+    private Long precio;
+
+    public Propiedad( String nombre, String direccion, String descripcion, Long precio){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Long precio) {
+        this.precio = precio;
+    }
+}
