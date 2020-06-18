bb08f0e93358711821741aa167e17d675724981b
Micaela Vandoni <mtvandoni@indracompany.com>
2020-06-18 19:15:09 -0300

    add test

---
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ControladorPerfilTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControladorPerfilTest {
+
+	@Test
+	public void ConsultarPropiedadesFavoritasDeUnUsuarioLogueado() {
+		
+	}
+
+}
